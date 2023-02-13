fc6731e588e1bb96d4cffe62ba8758d12bfafff5
李振宇 <407852978@qq.com>
2023-02-13 17:19:42 +0800

    新增一个默认配置

---
+++ R68S/diy-part2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# autocore
+sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_ipq807x||TARGET_mvebu||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+
+# Feeds.conf
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
+# 晶晨宝盒
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/happyplum/openwrt|g" package/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|http.*/library|https://github.com/happyplum/openwrt/opt/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|s9xxx_lede|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|.img.gz|..OPENWRT_SUFFIX|g" package/luci-app-amlogic/root/etc/config/amlogic
+
+# gzip
+# sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.12/g' feeds/packages/utils/gzip/Makefile
+# sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/packages/utils/gzip/Makefile
+# sed -i 's/PKG_HASH:=.*/PKG_HASH:=ce5e03e519f637e1f814011ace35c4f87b33c0bbabeec35baf5fbd3479e91956/g' feeds/packages/utils/gzip/Makefile
+
+# readd cpufreq for aarch64
+# sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
+# sed -i 's/services/system/g'  feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+
+./scripts/feeds update -a
+./scripts/feeds install -a
